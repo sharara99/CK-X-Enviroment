9c3777931aba323153d365134f4307bc373c636d
Nishan <nishanbeleri@gmail.com>
2025-08-26 14:12:26 +0530

    ADD: info for k8s command not being avilable on remote-desktop

--- remote-desktop/startup.sh
+++ remote-desktop/startup.sh
@@ -4,6 +4,7 @@
 echo "Starting CKAD VNC service at $(date)"
 
 echo "echo 'Use Ctrl + Shift + C for copying and Ctrl + Shift + V for pasting'" >> /home/candidate/.bashrc
+echo "alias kubectl='echo \"kubectl not available here. Solve this question on the specified instance\"'" >> /home/candidate/.bashrc
 
 # Run in the background - don't block the main container startup
 python3 /tmp/agent.py &
